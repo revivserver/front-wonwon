698d453b0e8568882b0dbc95b71e3d856da86da8
meawhung <meawhung@gmail.com>
2024-07-15 16:56:06 +0900

    add cancel on Modal

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -18,6 +18,7 @@ const Modal = ({
           <h3 class="text-xl text-brown-mid font-kanit">{textHeader}</h3>
           <button
             type="button"
+            onClick={onCancel}
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           >
             <svg

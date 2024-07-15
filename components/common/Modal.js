935af2c5eafa533a88375404ade102e8bf13d5da
meawhung <meawhung@gmail.com>
2024-07-15 18:21:27 +0900

    fix warning

--- components/common/Modal.js
+++ components/common/Modal.js
@@ -7,22 +7,22 @@ const Modal = ({
 }) => {
   return (
     <div
-      tabindex="-1"
+      tabIndex="-1"
       aria-hidden="true"
-      class="flex !m-0 fixed top-0 right-0 left-0 z-50 justify-center items-end w-full h-full  bg-opacity-75 backdrop-blur-lg"
+      className="flex !m-0 fixed top-0 right-0 left-0 z-50 justify-center items-end w-full h-full  bg-opacity-75 backdrop-blur-lg"
     >
       {/* <!-- Modal content --> */}
-      <div class="relative bg-butter-default rounded-t-[24px] shadow-md  px-10 pt-8 w-full">
+      <div className="relative bg-butter-default rounded-t-[24px] shadow-md  px-10 pt-8 w-full">
         {/* <!-- Modal header --> */}
-        <div class="pb-2 flex items-center justify-between border-b rounded-t border-dashed border-butter-dark">
-          <h3 class="text-xl text-brown-mid font-kanit">{textHeader}</h3>
+        <div className="pb-2 flex items-center justify-between border-b rounded-t border-dashed border-butter-dark">
+          <h3 className="text-xl text-brown-mid font-kanit">{textHeader}</h3>
           <button
             type="button"
             onClick={onCancel}
-            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
           >
             <svg
-              class="w-3 h-3"
+              className="w-3 h-3"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -30,19 +30,19 @@ const Modal = ({
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
               />
             </svg>
-            <span class="sr-only">Close modal</span>
+            <span className="sr-only">Close modal</span>
           </button>
         </div>
         {/* <!-- Modal body --> */}
-        <div class="py-4 text-brown-default font-kanit">{content}</div>
+        <div className="py-4 text-brown-default font-kanit">{content}</div>
         {/* <!-- Modal footer --> */}
-        <div class="flex justify-center pt-4 pb-8">
+        <div className="flex justify-center pt-4 pb-8">
           <button
             type="button"
             onClick={onSubmit}

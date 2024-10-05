892459999bcbd779e53f18d601e8ee2ccd9fd792
meawhung <meawhung@gmail.com>
2024-10-05 21:45:18 +0900

    fix width of new feature

--- pages/landing.js
+++ pages/landing.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from 'next/image';
 // import Link from 'next/link';
 import Landing_img from '@/public/landing-img.png';
 import wonwon_icon from '@/public/wonwon_icon.svg';
@@ -39,18 +39,20 @@ const LandingPage = ({}) => {
                 filter:
                   'brightness(0) saturate(100%) invert(78%) sepia(11%) saturate(4156%) hue-rotate(13deg) brightness(105%) contrast(72%)',
 
-                maxWidth: "100%",
-                height: "auto",
-              }} />
+                maxWidth: '100%',
+                height: 'auto'
+              }}
+            />
             <Image
               alt="description"
               src={Landing_img}
               width={210}
               height={210}
               style={{
-                maxWidth: "100%",
-                height: "auto",
-              }} />
+                maxWidth: '100%',
+                height: 'auto'
+              }}
+            />
           </div>
           <div className="flex flex-col items-center justify-center mt-4 text-xs font-normal text-brown-default font-kanit">
             <p className="text-xl font-medium text-center ">WonWon</p>
@@ -64,27 +66,27 @@ const LandingPage = ({}) => {
               <li className=" w-fit">รีวิวร้านให้ทุกคนรู้จัก</li>
             </ul>
           </div>
-          <div className="flex justify-center w-full pt-3 pb-1">
+          <div className="flex justify-center mt-3">
             <a href={url}>
               <button
                 type="button"
-                className="h-12 text-base font-normal rounded-full w-80 btn btn-primary bg-green-default text-brown-default font-kanit"
+                className=" h-12 rounded-full w-80 btn btn-primary bg-green-default text-brown-default font-kanit"
               >
                 เริ่มใช้งาน
               </button>
             </a>
           </div>
-          <div className="flex p-2 pb-10 space-x-2 text-butter-light font-kanit">
-            <div
+          <div className="flex justify-center mt-3 pb-3">
+            <button
               onClick={() =>
                 openInNewTab(
                   'https://revivcommunity.org/wonwon-future-features/'
                 )
               }
-              className=" cursor-pointer bg-[#835020] h-[48px] rounded-lg w-full justify-center items-center flex "
+              className=" bg-brown-mid h-12 rounded-lg w-80 text-butter-light font-kanit"
             >
-              <p>ติดตามฟีเจอร์ใหม่ๆ</p>
-            </div>
+              ติดตามฟีเจอร์ใหม่ๆ
+            </button>
           </div>
         </div>
       </div>

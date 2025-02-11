140d343acefaac57b0f77df66602368e254e227e
pomlim <passavich.lim@gmail.com>
2025-02-11 17:14:35 +0900

    add image selection icon border

--- components/shopDetail/shopImage.js
+++ components/shopDetail/shopImage.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 
 import {
   faCircleArrowLeft,
@@ -42,16 +42,25 @@ const ShopImage = ({ shop_images }) => {
 
       <div className="absolute flex items-center justify-between w-full px-5 transform -translate-y-1/2 top-1/2">
         <button
-          className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
           onClick={handleOnPrevClick}
         >
-          <FontAwesomeIcon icon={faCircleArrowLeft} />
+          <FontAwesomeIcon
+            icon={faCircleArrowLeft}
+            // TODO: wait feedback from Wai
+            // Without border
+            // className="text-green-default drop-shadow-lg"
+            // With border
+            className="text-green-default border-2 rounded-xl border-brown-default bg-brown-default"
+          />
         </button>
         <button
-          className="p-1 text-white transition bg-black bg-opacity-50 rounded-full cursor-pointer hover:bg-opacity-100"
           onClick={handleOnNextClick}
         >
-          <FontAwesomeIcon icon={faCircleArrowRight} />
+          <FontAwesomeIcon
+            icon={faCircleArrowRight}
+            // className="text-green-default drop-shadow-lg"
+            className="text-green-default border-2 rounded-xl border-brown-default bg-brown-default"
+          />
         </button>
       </div>
     </div>

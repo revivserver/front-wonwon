8ebc1c9212af149e5a650c69057e5453cbb50bbb
pomlim <passavich.lim@gmail.com>
2025-02-13 22:43:32 +0900

    feat: update contact components - add website and email link - display not have contact case

--- components/shopDetail/contact.js
+++ components/shopDetail/contact.js
@@ -4,6 +4,7 @@ import {
   faFacebook,
   faLine
 } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = ({ contact, onIconClick }) => {
   const contactsMethods = [
@@ -15,61 +16,95 @@ const Contact = ({ contact, onIconClick }) => {
     {
       name: 'facebook',
       data: contact.facebook?.length > 0 ? contact.facebook : null
+    },
+    {
+      name: 'webpage',
+      data: contact.webpage?.length > 0 ? contact.webpage : null
+    },
+    {
+      name: 'email',
+      data: contact.webpage?.length > 0 ? contact.webpage : null
     }
   ];
+  const isContactsMethodsNull = contactsMethods.every(
+    (contactsMethod) => contactsMethod.data === null
+  );
+
   return (
     <div className="pt-4">
       <p className="text-xs font-bold text-brown-default font-kanit">
         ช่องทางติดต่อ
       </p>
-      <div className="flex justify-around mt-2 p-4">
-        {contactsMethods.map((contactMethod, index) => {
-          if (!contactMethod.data) return null;
-          const icon =
-            contactMethod.name === 'instagram'
-              ? faInstagram
-              : contactMethod.name === 'line'
-              ? faLine
-              : faFacebook;
+      {isContactsMethodsNull ? (
+        <div className="pt-4 text-xs text-brown-default font-kanit text-center">
+          ไม่มีข้อมูล
+        </div>
+      ) : (
+        <div className="flex justify-around mt-2 p-4">
+          {contactsMethods.map((contactMethod, index) => {
+            if (!contactMethod.data) return null;
+            const icon =
+              contactMethod.name === 'instagram'
+                ? faInstagram
+                : contactMethod.name === 'line'
+                ? faLine
+                : contactMethod.name === 'webpage'
+                ? faGlobe
+                : contactMethod.name === 'email'
+                ? faEnvelope
+                : faFacebook;
 
-          const style =
-            contactMethod.name === 'instagram'
-              ? { color: '#E4405F' }
-              : contactMethod.name === 'line'
-              ? { color: '#00B900' }
-              : { color: '#1877F2' };
+            const style =
+              contactMethod.name === 'instagram'
+                ? { color: '#E4405F' }
+                : contactMethod.name === 'line'
+                ? { color: '#00B900' }
+                : contactMethod.name === 'webpage'
+                ? { color: '#443616' }
+                : contactMethod.name === 'email'
+                ? { color: '#443616' }
+                : { color: '#1877F2' };
 
-          const linkToContact =
-            contactMethod.name === 'line'
-              ? `https://line.me/R/ti/p/${contactMethod.data[0]}`
-              : contactMethod.data[0];
+            const linkToContact =
+              contactMethod.name === 'line'
+                ? `https://line.me/R/ti/p/${contactMethod.data[0]}`
+                : contactMethod.name === 'webpage'
+                ? `http://${contactMethod.data[0]}`
+                : contactMethod.name === 'email'
+                ? `mailto:${contactMethod.data[0]}`
+                : contactMethod.data[0];
 
-          if (contactMethod.data.length > 1) {
-            return (
-              <FontAwesomeIcon
-                key={`contact-icon-${index}`}
-                className="cursor-pointer "
-                icon={icon}
-                size="xl"
-                inverse
-                style={style}
-                onClick={() => onIconClick(contactMethod.name)}
-              />
-            );
-          } else {
-            return (
-              <a
-                key={`contact-icon-${index}`}
-                href={linkToContact}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon icon={icon} size="xl" inverse style={style} />
-              </a>
-            );
-          }
-        })}
-        {/* {emails ? (
+            if (contactMethod.data.length > 1) {
+              return (
+                <FontAwesomeIcon
+                  key={`contact-icon-${index}`}
+                  className="cursor-pointer "
+                  icon={icon}
+                  size="xl"
+                  inverse
+                  style={style}
+                  onClick={() => onIconClick(contactMethod.name)}
+                />
+              );
+            } else {
+              return (
+                <a
+                  key={`contact-icon-${index}`}
+                  href={linkToContact}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={icon}
+                    size="xl"
+                    inverse
+                    style={style}
+                  />
+                </a>
+              );
+            }
+          })}
+          {/* {emails ? (
           <a href={`mailto:${emails[0]}`}>
             <FontAwesomeIcon icon={faEnvelope} size="xl" />
           </a>
@@ -79,7 +114,8 @@ const Contact = ({ contact, onIconClick }) => {
             <FontAwesomeIcon icon={faGlobe} size="xl" />
           </a>
         ) : null} */}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
